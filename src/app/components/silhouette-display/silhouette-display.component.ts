5833184e15cc312cc93d8d77d06d82afd15af3ff
NicolasJorquera <nicolas.jorqueramartinez@gmail.com>
2025-03-24 22:58:16 -0300

    [ADD] 3d model

--- src/app/components/silhouette-display/silhouette-display.component.ts
+++ src/app/components/silhouette-display/silhouette-display.component.ts
@@ -1,23 +1,21 @@
-import { Component } from '@angular/core';
-import { WatchService } from '../../services/watch.service';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WatchService } from '../../services/watch.service';
 
 @Component({
   selector: 'app-silhouette-display',
   standalone: true,
   templateUrl: './silhouette-display.component.html',
-//   styleUrls: ['./silhouette-display.component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] // 👈 Add this line
 })
 export class SilhouetteDisplayComponent {
   watchSize = { width: 40, height: 40, wristSize: 180 };
 
   constructor(private watchService: WatchService) {
-
     this.watchService.watchSize$.subscribe(size => {
       console.log(size);
       this.watchSize = size;
     });
   }
-
 }
